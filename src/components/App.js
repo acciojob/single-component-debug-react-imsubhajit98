971bf30967444ed008c802544d646348d42a7a6f
imsubhajit98 <subhajit06521@gmail.com>
2023-03-24 01:38:04 +0530

    Ram

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react"
 
+function App(){
+    return(
+        <React.Fragment>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </React.Fragment>
+    )
+}
+
+export default App;
