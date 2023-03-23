beb9d6b8ab9f68fa26f0515325ab9e3da7088989
imsubhajit98 <subhajit06521@gmail.com>
2023-03-24 01:50:32 +0530

    Ram

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react"
-
+import Para from "./Para";
 function App(){
     return(
         <div>
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            <para/>
         </div>
     )
 }
---
+++ src/components/Para.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Para(){
+    return(
+        <React.Fragment>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </React.Fragment>
+    )
+}
+
+export default Para;
